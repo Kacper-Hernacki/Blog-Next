90ddf485334d0776b4ea7afde031de3938a3f968
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-03-18 10:21:36 +0100

    youtube mobile 📱

--- components/EmbeddedYoutube.tsx
+++ components/EmbeddedYoutube.tsx
@@ -9,8 +9,8 @@ function EmbeddedYoutube() {
   };
 
   const opts: YouTubeProps["opts"] = {
-    height: isMobile ? "150" : "390",
-    width: isMobile ? "200" : "640",
+    height: isMobile ? "180" : "390",
+    width: isMobile ? "286" : "640",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
@@ -20,10 +20,11 @@ function EmbeddedYoutube() {
   return (
     <div className="bg-gray-800">
       <div className="w-full h-24 absolute bg-gradient-to-b from-gray-600" />
-      <h1 className="absolute w-full text-center text-4xl font-bold p-6 text-white max-[800px]:pb-8">Latest YouTube
+      <h1 className="absolute w-full text-center text-4xl font-bold p-6 text-white max-[800px]:pb-8 mb-8">Latest YouTube
         Videos</h1>
-      <div className="max-w-2xl p-6 pb-8 mx-auto pt-28">
-        <YouTube videoId="gl6jD4qIO_8" opts={opts} onReady={onPlayerReady} />;
+      <div className="grid place-items-center max-w-2xl p-6 mt-4 pb-8 mx-auto pt-28">
+        <YouTube className="mt-4" videoId="gl6jD4qIO_8" opts={opts} onReady={onPlayerReady} />;
+        <YouTube className="mt-4" videoId="sCe_ewIBnYQ" opts={opts} onReady={onPlayerReady} />;
       </div>
       <div className="w-full h-32 bg-gradient-to-t from-gray-600" />
     </div>
