53c758040556c5fa01e929fc340139078a69f7e6
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-03-18 22:02:13 +0100

    fixes

--- app/(user)/thanks/page.tsx
+++ app/(user)/thanks/page.tsx
@@ -18,14 +18,14 @@ const Model = () => {
 export default function Thanks() {
   return (
     <Container className="flex-col w-full justify-center mt-96 pt-10">
-      <Banner className="absolute w-full left-1/2 -translate-x-1/2 sm:mx-auto max-[600px]:m-6 text-black flex-col w-max-md mx-10 mb-4 max-w-screen-md align-middle justify-center  z-10 p-5">
-        <h1 className="text-3xl font-bold mt-20 max-[600px]:mt-5 text-center w-full drop-shadow-lg">
+      <Banner className="absolute top-2/3 w-full left-1/2 -translate-x-1/2 sm:mx-auto max-[600px]:m-0 text-black flex-col w-max-md mx-10 mb-4 max-w-screen-md align-middle justify-center  z-10 p-5">
+        <h1 className="text-3xl font-bold text-center w-full drop-shadow-lg">
           Huge Thanks for signing to my Newsletter 🎊
         </h1>
         <h3 className="text-xl font-bold text-center w-full drop-shadow-lg">More aweosme content soon</h3>
 
       </Banner>
-      <ThreeDContainer className="mt-20 absolute w-full top-1/2 left-1/2 -translate-x-1/2 grid place-items-center cursor-grabbing -mt-52 h-96">
+      <ThreeDContainer className="mt-20 absolute w-full top-2/3 left-1/2 -translate-x-1/2 grid place-items-center cursor-grabbing -mt-52 h-96">
         <Canvas className="h-96" shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
           <ambientLight intensity={0.7} />
           <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
