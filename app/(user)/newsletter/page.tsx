9d998d07254d8b1e53b77d59b44dbc727b474dca
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2023-03-19 09:14:25 +0100

    newsletter glitches

--- app/(user)/newsletter/page.tsx
+++ app/(user)/newsletter/page.tsx
@@ -2,7 +2,7 @@ import NewsletterForm from "../../../components/NewsletterForm";
 
 export default function NewsletterPage () {
     return (
-      <div className="mt-80"><NewsletterForm/></div>
+      <div className="mt-96 mb-24"><NewsletterForm/></div>
 
     );
 }
